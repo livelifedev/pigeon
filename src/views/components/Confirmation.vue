6d321e833b6b085d2887db91c13a0cab8c47715b
John <johnrubio93@gmail.com>
2020-03-23 20:52:12 +1100

    installed moment js

--- src/views/components/Confirmation.vue
+++ src/views/components/Confirmation.vue
@@ -4,8 +4,16 @@
     <el-row>
       <el-col :span="24">
         <el-card class="box-card">
-          <h2 class='card-title'>BIRTH AND GUILD CERTIFICATE</h2>
+          <h2 class="card-title">BIRTH AND GUILD CERTIFICATE</h2>
           <h3>Guild of Pigeon Breeders</h3>
+
+          <div>DOB: {{ currentDate }}</div>
+          <div>Name:</div>
+          <div>Clan:</div>
+          <div>Gender:</div>
+          <div>Region:</div>
+          <div>Breed:</div>
+          <div>Element:</div>
         </el-card>
       </el-col>
     </el-row>
@@ -14,12 +22,18 @@
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
   name: 'Confirmation',
   data() {
     return {
+      currentDate: null,
       formDetails: {}
     };
+  },
+  created() {
+    this.currentDate = moment().format('MMMM DD, YYYY');
   },
   methods: {
     onSubmit() {}
