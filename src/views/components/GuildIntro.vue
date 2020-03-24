6c646ebf022cafe2c2a1bfbb1b5fc02c32b9bf4d
John <johnrubio93@gmail.com>
2020-03-24 19:01:06 +1100

    updated intro text

--- src/views/components/GuildIntro.vue
+++ src/views/components/GuildIntro.vue
@@ -2,6 +2,7 @@
   <div class="guild-intro-container">
     <h1>Introduction to the guild.</h1>
     <p>{{introText}}</p>
+    <p>{{secondaryText}}</p>
   </div>
 </template>
 
@@ -10,7 +11,8 @@ export default {
   name: 'GuildIntro',
   data() {
     return {
-      introText: "The Guild of Pigeon Breeders (GPB) is an ancient association formed with the intent of unlocking the secrets and furthering the evolution of the mystical creature, Pigeon.",
+      introText: "The Guild of Pigeon Breeders (GPB) is an ancient association formed to pursue the secrets and further the evolution of the mystical creature, Pigeon.",
+      secondaryText: "Your potential to be an outstanding pigeon breeder has been recognized and we welcome you to join our ranks."
     };
   },
   methods: {
