2739b98f3359049d306cce43ed1980218e350a2a
John <johnrubio93@gmail.com>
2020-03-29 19:22:38 +1100

    updated time select logic

--- src/views/components/features/CareFeed.vue
+++ src/views/components/features/CareFeed.vue
@@ -6,12 +6,19 @@
       appetite and requires <span class="info-text">3x feeding</span>.
     </p>
 
-    <el-button class="action-button" type="success" size="small">
+    <el-button
+      class="action-button"
+      type="success"
+      size="small"
+      :disabled="!feedingSchedule"
+    >
       FEED
     </el-button>
     <h4 class="feature-subtitle">
       Feeding schedule
       <el-button
+        v-if="!canEdit"
+        @click="canEdit = true"
         class="edit-button"
         icon="el-icon-edit"
         circle
@@ -19,22 +26,39 @@
       ></el-button>
     </h4>
 
+    <p class="sub-text" v-if="!feedingSchedule">
+      Please create a feeding schedule.
+    </p>
+
     <el-form ref="form" :model="form">
-      <el-form-item v-for="meal in appetite" :key="meal" :prop="`meal${meal}`">
+      <el-form-item
+        v-for="meal in appetite"
+        :key="meal"
+        :prop="`meal${meal}`"
+        :show-message="false"
+        required
+      >
         <el-time-select
           v-model="form[`meal${meal}`]"
           :picker-options="{
-            start: '00:00',
+            start: '01:00',
             step: '01:00',
-            end: '23:00'
+            end: '24:00',
+            minTime: checkMinTime(meal),
+            maxTime: checkMaxTime(meal)
           }"
           :placeholder="`meal ${meal}`"
+          :clearable="false"
+          :readonly="!canEdit"
+          :disabled="disableTimeSelect(meal)"
         >
         </el-time-select>
       </el-form-item>
 
-      <el-form-item>
-        <el-button type="primary" size="small" @click="test">Save</el-button>
+      <el-form-item v-if="canEdit">
+        <el-button type="primary" size="small" @click="handleOnSave"
+          >Save</el-button
+        >
       </el-form-item>
     </el-form>
   </div>
@@ -49,23 +73,44 @@ export default {
     return {
       appetite: 3,
 
-      form: {},
-      time: null
+      feedingSchedule: JSON.parse(localStorage.getItem('feedingSchedule')),
+
+      canEdit: false,
+      form: {}
     };
   },
   created() {
-    this.form = { ...this.form, ...this.createTimeSlots(this.appetite) };
+    if (this.feedingSchedule) {
+      this.form = this.feedingSchedule;
+    } else {
+      this.canEdit = true;
+    }
     console.log(moment().format('H'));
   },
   methods: {
-    createTimeSlots(n) {
-      const timeSlots = {};
-      for (let i = 0; i < n; i++) timeSlots[`meal${i + 1}`] = null;
-      return timeSlots;
-    },
-    test() {
-      console.log(this.form.meal1.substring(0, 2));
+    handleOnSave() {
+      // console.log(this.form.meal1.substring(0, 2));
       // form.meal1.substring(0,2)
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          localStorage.setItem('feedingSchedule', JSON.stringify(this.form));
+          this.feedingSchedule = this.form;
+          this.canEdit = false;
+        }
+      });
+    },
+    disableTimeSelect(meal) {
+      if (meal > 1) return !this.form[`meal${meal - 1}`];
+      return false;
+    },
+    checkMinTime(meal) {
+      if (meal > 1) return this.form[`meal${meal - 1}`];
+      return '00:00';
+    },
+    checkMaxTime(meal) {
+      const nextTime = this.form[`meal${meal + 1}`];
+      if (nextTime) return this.form[`meal${meal + 1}`];
+      return null;
     }
   }
 };
@@ -92,5 +137,8 @@ export default {
 }
 .el-form-item {
   margin-bottom: 5px;
+}
+.sub-text {
+  font-size: 0.8rem;
 }
 </style>
